74b7e4b6c4d87dad82126bd7ebc4e7f39d1120ca
KarinaMadrigal <101362675+KarinaMadrigal@users.noreply.github.com>
2022-11-21 23:21:19 -0600

    Inicio

---
+++ src/simulacion/Simulacion.java
@@ -0,0 +1,16 @@
+package simulacion;
+
+/**
+ *
+ * @author Grupo 2
+ */
+public class Simulacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Se adjunta documento
+    }
+    
+}
